d6fdd23bd0a35d9e63fb152254e7cd002c935d51
Fernando Marca Magalhães <59258828+fernandomarca@users.noreply.github.com>
2021-05-10 23:37:46 -0300

    query examples

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,8 +1,8 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from "typeorm";
 
-import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
-import { User } from '../../entities/User';
-import { IUsersRepository } from '../IUsersRepository';
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
 
 export class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
@@ -15,16 +15,28 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOne({
+      where: { id: user_id },
+      relations: ["games"],
+    });
+    return user as User;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    const users = await this.repository.query(`
+      SELECT * FROM users ORDER BY first_name ASC;
+    `); // Complete usando raw query
+    return users;
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const users = this.repository.query(`
+    SELECT first_name, last_name, email FROM users 
+    WHERE lower(first_name)=lower('${first_name}') AND lower(last_name)=lower('${last_name}');
+    `); // Complete usando raw query
+    return users;
   }
 }
